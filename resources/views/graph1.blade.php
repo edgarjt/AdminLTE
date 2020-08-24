ff7aa3fbab2c81d8786740a625a7fdf2f51582e7
edgarjt <edgarjt97@gmail.com>
2020-08-24 09:54:11 -0500

    mi first admin-lte

--- resources/views/graph1.blade.php
+++ resources/views/graph1.blade.php
@@ -1,6 +1,4 @@
 @extends('layouts.app')
 @section('content')
-    <div class="content-wrapper">
-        <h1>Grafica 1</h1>
-    </div>
+    <h1>Grafica 1</h1>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,5 @@
 @extends('layouts.app')
 @section('content')
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <h1>Test dasgboard</h1>
-    </div>
-
-    <!-- -------------------- Control Sidebar ---------------------------------------------------------------- -->
-{{--    <aside class="control-sidebar control-sidebar-dark min-vh-100">
-        <!-- Create the tabs -->
-        <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
---}}{{--            <li><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>--}}{{--
-            <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
-        </ul>
-        <!-- Tab panes -->
-        <div class="tab-content">
-            <!-- Home tab content -->
-            <div class="tab-pane vh-100" id="control-sidebar-home-tab">
-
-            </div>
-        </div>
-    </aside>--}}
+    <h1>Content test</h1>
 @endsection
 
