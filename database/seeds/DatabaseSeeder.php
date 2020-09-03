9eda6fd088a850b1feb2a2b5543d7d3f7062894e
edgarjt <edgarjt97@gmail.com>
2020-09-03 03:22:46 -0500

    descomment seeder municipios

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserSeeder::class);
-        //$this->call(MunicipiosSeeder::class);
+        $this->call(MunicipiosSeeder::class);
         $this->call(SubDelegacionesSeeder::class);
         $this->call(EnfermedadesSeeder::class);
         $this->call(EmergenciasSeeder::class);
