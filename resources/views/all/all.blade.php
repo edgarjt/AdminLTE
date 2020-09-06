1ebb8133cba13337f6052d0357d7c725846b7330
edgarjt <edgarjt97@gmail.com>
2020-09-06 03:07:38 -0500

    graph all view

--- resources/views/all/all.blade.php
+++ resources/views/all/all.blade.php
@@ -3,7 +3,7 @@
     <section class="content-header">
         <h1>
             Estadísticas:
-            <small>General fallecidos</small>
+            <small>General</small>
         </h1>
         <ol class="breadcrumb">
             <li><a href="{{route('home')}}"><i class="fa fa-dashboard"></i> Inicio</a></li>
@@ -11,11 +11,25 @@
     </section>
 
     <section class="content">
+
+        <div class="form-inline" style="margin-top: 5px; margin-bottom: 20px">
+
+            <input type="text" class="form-control" id="year" placeholder="Ingrese el año">
+
+            <select class="form-control" id="type-graph">
+                <option value="bar">Barra</option>
+                <option value="doughnut">Pastel</option>
+                <option value="polarArea">Area polar</option>
+            </select>
+
+            <button class="btn btn-default btn-sm" id="graphAll">Generar</button>
+        </div>
+
         <div class="row">
             <div class="col-md-6">
-                <div class="box box-warning">
+                <div class="box box-danger">
                     <div class="box-header with-border">
-                        <h3 class="box-title">Emergencias</h3>
+                        <h3 class="box-title">Gráfica pacientes</h3>
 
                         <div class="box-tools pull-right">
                             <button type="button" class="btn btn-box-tool" data-widget="collapse">
@@ -28,27 +42,6 @@
                     </div>
                     <div class="box-body">
                         <div class="chart">
-                            <div class="form-group form-group-sm">
-                                <div class="form-inline">
-                                    <select class="form-control" id="select-month">
-                                        @foreach($subdelegaciones as $subdelegacion)
-                                            <option value="{{$subdelegacion->sub_id}}">
-                                                {{$subdelegacion->sub_nombre}}
-                                            </option>
-                                        @endforeach
-                                    </select>
-
-                                    <select class="form-control" id="type-graph">
-                                        <option value="bar">Barra</option>
-                                        <option value="doughnut">Pastel</option>
-                                        <option value="polarArea">Area polar</option>
-                                    </select>
-
-                                    <button class="btn btn-default btn-sm" id="graphMonth">Generar</button>
-                                </div>
-
-                            </div>
-
                             <div style="text-align: center" class="d-none load">
                                 <img src="{{asset('img/load.gif')}}" alt="Load" width="30">
                             </div>
@@ -63,7 +56,7 @@
             <div class="col-md-6">
                 <div class="box box-danger">
                     <div class="box-header with-border">
-                        <h3 class="box-title">Fallecidos</h3>
+                        <h3 class="box-title">Gráfica fallecidos</h3>
 
                         <div class="box-tools pull-right">
                             <button type="button" class="btn btn-box-tool" data-widget="collapse">
@@ -76,7 +69,7 @@
                     </div>
                     <div class="box-body">
                         <div class="chart">
-                            <div style="text-align: center" class="d-none load">
+                            <div style="text-align: center" class="d-none load-deced">
                                 <img src="{{asset('img/load.gif')}}" alt="Load" width="30">
                             </div>
                             <div id="registros-decesos"></div>
@@ -89,3 +82,4 @@
         </div>
     </section>
 @endsection
+
