e4f36c171c1a3e2939783f6f6e222df8d5738692
edgarjt <edgarjt97@gmail.com>
2020-09-09 01:14:11 -0500

    icon logo

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Cruz Roja Tab</title>
+    <link rel="icon" type="image/x-icon" href="{{asset('img/logo1.ico')}}">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
