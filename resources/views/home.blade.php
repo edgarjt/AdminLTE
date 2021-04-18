7e6f0e38d04d6ba09a1acad64e4ced5099400b71
edgarjt <edgarjt97@gmail.com>
2021-04-18 15:25:38 -0500

    delete date initial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,4 @@
 @extends('layouts.app')
 @section('content')
-    <p class="date-home"><strong>Fecha:</strong> {{\Carbon\Carbon::now()->format('d-M-Y')}}</p>
-    <p class="date-home"><strong>Nombre:</strong> {{Auth::User()->name}} {{Auth::User()->surname}}</p>
-    <p class="date-home"><strong>Turno:</strong> Vespertino</p>
 @endsection
 
